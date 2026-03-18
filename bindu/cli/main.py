372dc8a7c5f702e2202f52026d2c82a24358b410
Avngrstark62 <thakurabhijeetsingh79@gmail.com>
2026-03-18 19:12:33 +0530

    minor fixes in onboard cli

--- bindu/cli/main.py
+++ bindu/cli/main.py
@@ -1,13 +1,10 @@
 import typer
 
-from bindu.cli import onboard, db, train, canary
+from bindu.cli import onboard
 
 app = typer.Typer(help="Bindu CLI")
 
 app.add_typer(onboard.app, name="onboard")
-app.add_typer(db.app, name="db")
-app.add_typer(train.app, name="train")
-app.add_typer(canary.app, name="canary")
 
 def main():
     app()
